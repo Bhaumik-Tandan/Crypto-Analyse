851f7fa9fa33ae3ac61f9ea5800b08ee531d6351
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-05-10 11:37:17 +0530

    [add] sign in button by default

--- src/app/components/Auth.tsx
+++ src/app/components/Auth.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import {useSession, signIn, signOut} from 'next-auth/react';
-function Auth() {
-    const {data: session} = useSession();
-    if(session)
+import React, { useEffect } from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
-  return (
-    <button onClick={()=>signOut()}>{session.user?.email}</button>
-  )
-    else
+function Auth() {
+    const { data: session, status } = useSession();
+
+    useEffect(() => {
+        // Check if the session is not active and the status is not loading
+        if (status === 'unauthenticated') {
+            signIn();  // Automatically triggers sign-in
+        }
+    }, [status]); // Dependency array includes status to react on its changes
+
+    if (session) {
         return (
-        <button onClick={()=>signIn()}>Login</button>
+            <button onClick={() => signOut()}>{session.user?.email}</button>
         );
+    } else {
+        return (
+            <button onClick={() => signIn()}>Login</button>
+        );
+    }
 }
 
-export default Auth
+export default Auth;

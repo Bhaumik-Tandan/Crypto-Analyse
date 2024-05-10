81008dd3b46cea07a1c0fd3acbfe620948c2022e
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-05-10 18:32:55 +0530

    [FIX] api call request

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,12 +1,10 @@
-"use server";
 import {  NextApiResponse } from 'next';
 import { NextResponse,NextRequest } from 'next/server';
 import searchSymbol from '@/app/utility/searchSymbol';
-
+export const dynamic="force-dynamic";
 export const GET =async (req: NextRequest, res: NextApiResponse) => {
   try {
-    const url = new URL(req.url);
-    const symbol = url.searchParams.get('q');
+    const symbol = req.nextUrl.searchParams.get('q');
     const data = await searchSymbol(symbol||"");
     return NextResponse.json( data);
   } catch (error) {
--- src/app/api/stocks/route.ts
+++ src/app/api/stocks/route.ts
@@ -1,11 +1,10 @@
-"use server";
 import {  NextApiResponse,NextApiRequest } from 'next';
 import { NextResponse,NextRequest } from 'next/server';
 import getStockData from "../../utility/getStockData";
+export const dynamic="force-dynamic";
 export const GET =async (req: NextRequest, res: NextApiResponse) => {
   try {
-    const url = new URL(req.url);
-    const symbol = url.searchParams.get('symbol');
+    const symbol = req.nextUrl.searchParams.get('symbol');
     const data = await getStockData(symbol||"XBTUSD");
     return NextResponse.json( data);
   } catch (error) {

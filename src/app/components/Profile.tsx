5f34162b3185d0614cb18d47cf448275008f085b
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-05-10 12:21:00 +0530

    [add] made image circular

---
+++ src/app/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSession, signOut } from 'next-auth/react';
+
+function Profile() {
+    const { data: session } = useSession();
+    return (
+        <div style={{
+            width: '100%',
+            display: 'flex',            
+            justifyContent: 'flex-end'
+        }}>
+            <img src={session.user?.image} alt='profile' onClick={() => signOut()} style={styles.profileImage} />
+        </div>
+    )
+}
+
+export default Profile;
+
+const styles = {
+    profileImage: {
+        width: '40px',
+        height: '40px',
+        borderRadius: '50%',
+        objectFit: 'cover',
+        margin: '10px', 
+    }
+};
